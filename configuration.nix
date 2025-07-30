9b7f43fe644916920ee4b5e353136ee56e65910a
Roam45 <amorr34@mybce.catholic.edu.au>
2025-07-31 09:18:34 +1000

    update

---
+++ configuration.nix
@@ -0,0 +1,229 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos-btw"; # Define your hostname.
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  time.timeZone = "Australia/Brisbane";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  environment.sessionVariables = {
+    WLR_NO_HARDWARE_CURSORS = "1";
+    NIXOS_OZONE_WL = "1";
+  };
+
+  hardware = {
+    opengl.enable = true;
+    nvidia.modesetting.enable = true;
+  };
+
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+ #	services.xserver = {
+#		enable = true;
+#		windowManager.qtile.enable = true;
+#		displayManager.sessionCommands = ''
+#			xwallpaper --zoom ~/walls/castle.jpg
+#			xset r rate 200 35 &
+#		'';
+#	};
+
+#	services.picom = {
+#		enable = true;
+#		backend = "glx";
+#		fade = true;
+#	};
+
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
+
+  services.flatpak.enable = true;
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+  # Enable sound.
+  # hardware.pulseaudio.enable = true;
+  # OR
+  # services.pipewire = {
+  #   enable = true;
+  #   pulse.enable = true;
+  # };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.styrofoam = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      tree
+    ];
+  };
+
+  programs.firefox.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    alacritty
+    btop
+    pcmanfm
+    rofi
+    rofi-wayland
+    git
+    pfetch
+    micro
+    fastfetch
+    hyprland
+    waybar
+    mako
+    libnotify
+    swww
+    cava
+    kitty
+    networkmanagerapplet
+    pavucontrol
+    vanilla-dmz
+    rofi-power-menu
+    pipes
+    papirus-icon-theme
+    fira-code-nerdfont
+    lxappearance
+    rofi-power-menu
+    breeze-qt5
+    breeze-icons
+    breeze-gtk
+  ];
+
+  environment.variables = {
+    XCURSOR_THEME = "Breeze";
+    XCURSOR_SIZE = "24";
+  };
+
+  services.xserver.displayManager.sessionCommands = ''
+    export XCURSOR_PATH=${pkgs.vanilla-dmz}/share/icons
+    export XCURSOR_THEME=Vanilla-DMZ
+  '';
+
+ fonts = {
+    packages = with pkgs; [
+      # Main fonts
+      monaspace
+      fira-code
+
+      # Nerd Fonts (new style)
+      nerd-fonts.fira-code
+      nerd-fonts.monaspace
+      nerd-fonts.hack
+      nerd-fonts.jetbrains-mono
+
+      # Alternative if you want ALL Nerd Fonts (not recommended)
+      # (builtins.attrValues nerd-fonts)
+
+      # Emoji support
+      noto-fonts-emoji
+      twemoji-color-font
+    ];
+
+    fontconfig = {
+      defaultFonts = {
+        monospace = [ "Monaspace Nerd Font Mono" "FiraCode Nerd Font Mono" ];
+        sansSerif = [ "Fira Code" ];
+        emoji = [ "Noto Color Emoji" ];
+      };
+    };
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
